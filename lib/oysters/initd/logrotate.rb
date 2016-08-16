f6f9dd86ca0710f898b1b505be157ddf354d7c8f
Eugene Baranov <Eugene_Baranov@cable.comcast.com>
2016-08-16 13:09:57 +0300

    use wildcard symbol to catch all log files

--- lib/oysters/initd/logrotate.rb
+++ lib/oysters/initd/logrotate.rb
@@ -7,29 +7,16 @@ Oysters.with_configuration do
     namespace :logrotate do
       desc 'Install logrotate config'
       task :install, roles: :app do
-        log_path = [ Pathname.new(shared_path).join('log', "#{rails_env}.log").to_s ]
-
-        if application == 'oiv-ui'
-          %W(#{rails_env}_delayed_job.log #{rails_env}_security_audit.log unicorn_#{rails_env}.stderr.log skylight.log).each do |log|
-            log_path << Pathname.new(shared_path).join('log', log).to_s
-          end
-        end
-
-        if application == 'oiv-api'
-          %W(skylight.log).each do |log|
-            log_path << Pathname.new(shared_path).join('log', log).to_s
-          end
-        end
+        log_path = Pathname.new(shared_path).join('log', "*.log").to_s
 
         conf_path = "/etc/logrotate.d/#{application}_logs"
-        log_path  = log_path.join(' ')
         tmp_file  = "/tmp/#{application}_logs_#{rand}"
         location  = File.expand_path('../../templates/logrotate_config.erb', __FILE__)
         config    = ERB.new(File.read(location))
 
         put config.result(binding), tmp_file
-        sudo "mv #{tmp_file} #{conf_path}"
-        sudo "logrotate #{conf_path}"
+        sudo "mv #{tmp_file} #{conf_path}", pty: true, shell: :bash
+        sudo "logrotate #{conf_path}", pty: true, shell: :bash
       end
     end
 
